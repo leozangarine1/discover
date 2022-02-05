0630eed3b020023bbb013444b921780ada87898e
leozangarine1 <92650666+leozangarine1@users.noreply.github.com>
2022-02-05 01:47:01 -0300

    Strings

---
+++ JavaScript/4.0-josé.js
@@ -0,0 +1,16 @@
+let nome = 'José'
+let poema = `
+E agora, ${nome}?
+A festa acabou,
+a luz apagou, 
+o povo sumiu, 
+a noite esfriou,
+e agora, ${nome}?
+e agora, você?
+você que é sem nome,
+que zomba dos outros,
+você que faz versos,
+que ama, protesta?
+e agora, ${nome}? 
+`
+console.log(poema)
