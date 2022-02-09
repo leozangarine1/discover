aac5d902f4aa065078787bcc7175dfa7a0575564
leozangarine1 <92650666+leozangarine1@users.noreply.github.com>
2022-02-09 02:02:33 -0300

    Prosseguindo com os estudos de JS

---
+++ JavaScript/3.0-PraticandoEAvançando/teste.js
@@ -0,0 +1,11 @@
+
+
+let name, age, isHuman
+name = 'Leonardo'
+age = 20
+isHuman = true
+//concatenação
+console.log('o ' + name + ' tem ' + age + ' anos.' )
+
+//interpolando valores
+console.log(`o ${name} tem ${age} anos.`)
