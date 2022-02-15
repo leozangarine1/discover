1f4ac1f4ab80886722d0e88f7b2ed932b8ce8058
leozangarine1 <92650666+leozangarine1@users.noreply.github.com>
2022-02-15 01:08:55 -0300

    Funções

--- JavaScript/4.0-Functions/teste.js
+++ JavaScript/4.0-Functions/teste.js
@@ -1,4 +1,10 @@
-function multiply(a, b){
-    a * b
-  }
-  
+// function expression
+            // function anonymous
+            
+            // parâmetros (parameter)
+            
+            const sum = function(number1, number2) {
+              console.log(number1 + number2)
+          }
+
+          sum(9, 10) // arguments - argumentos
