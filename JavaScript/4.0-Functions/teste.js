f2d3f3f47a639c4eb17fb5a9dd5805c7baacdc0f
leozangarine1 <92650666+leozangarine1@users.noreply.github.com>
2022-02-16 13:07:17 -0300

    Devagar e sempre.

--- JavaScript/4.0-Functions/teste.js
+++ JavaScript/4.0-Functions/teste.js
@@ -1,7 +1,18 @@
- // arrow function
+/*
+    Function() constructor
 
- const sayMyName = (name) => {
-    console.log(name)
-}
+    * expressão new
+    * criar um novo objeto
+    * this keyword */
 
-sayMyName('Leo')
+    function Person(name) {
+        this.name = name
+        this.walk = function(){
+            return this.name + " está andando"
+        }
+    }
+
+    const leo = new Person('Leo')
+    const lahana = new Person('Lahana')
+    console.log(leo.walk())
+    console.log(lahana.walk())
