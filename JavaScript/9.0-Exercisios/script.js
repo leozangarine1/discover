b355452bc5336af435ed10bfe5e756c966afaf99
leozangarine1 <92650666+leozangarine1@users.noreply.github.com>
2022-03-15 13:20:05 -0300

    Notas escolas e Fluxo de caixa

--- JavaScript/9.0-Exercisios/script.js
+++ JavaScript/9.0-Exercisios/script.js
@@ -1,37 +1,33 @@
-/* 
-Crie um algoritmo que transforme as notas do sistema
-numérico para sistema de notas em caracteres tipo A B C
-
-* de 90 para cima -   A
-* entre 80 - 89   -   B
-* entre 70 - 79   -   C
-* entre 60 - 69   -   D
-* menor que 60    -   F 
-*/
-
-function getScore(notaNum){
-let nota = ""
-
-if(notaNum >= 90 && notaNum <= 100){
-    nota = 'A'
-} else if(notaNum >= 80 && notaNum <= 89) {
-    nota = 'B'
-} else if(notaNum >= 70 && notaNum <= 79) {
-    nota = 'C'
-} else if(notaNum >= 60 && notaNum <= 69) {
-    nota = 'D'
-} else if(notaNum < 60 && notaNum >= 0) {
-    nota = 'F'
-} else {
-    console.log('Nota inválida')
+let family = {
+    incomes: [11200, 1200, 250.43, 360.45],
+    expenses: [320.34, 128.45, 176.87, 1450.00, 5000]
 }
 
-console.log(nota)
+
+
+function sum(array) {
+    let total = 0;
+
+    for(let value of array) {
+        total += value
+    }
+    return total
 }
 
-getScore(90)
-getScore(52)
-getScore(877)
-getScore(36)
-getScore(78)
-getScore(89)
+function calculateBalance() {
+    const calculateIncomes = sum(family.incomes)
+    const calculateExpenses = sum(family.expenses)
+
+    const total = calculateIncomes - calculateExpenses;
+
+    const itsOk = total >= 0
+
+    let balanceText = "Negativo"
+    if (itsOk) {
+        balanceText = "Positivo"
+    }
+    console.log(`Seu saldo é 
+    ${balanceText}: R$ ${total.toFixed(2)}`)
+}
+
+calculateBalance()
